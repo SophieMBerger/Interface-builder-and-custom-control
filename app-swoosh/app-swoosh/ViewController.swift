1837d9589485ea7804fc35d4b449db818f5b26dc
SophieMBerger <berger.m.sophie@gmail.com>
2018-08-07 16:01:00 +0200

    UI Constraints added

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -12,9 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet weak var BGImg: UIImageView!
+    @IBOutlet weak var swoosh: UIImageView!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
